5783851d6a8f7e077931bee4788fe786e6a40c5b
Alberto Martín <alberto.martin@bitergia.com>
2015-09-22 15:33:38 +0200

    Reviews feeder from underscore to camel case

--- server/feeders/reviews_generator.js
+++ server/feeders/reviews_generator.js
@@ -1,7 +1,7 @@
 /*
  * reviews_generator.js
  * Copyright(c) 2015 Bitergia
- * Author: Alvaro del Castillo <acs@bitergia.com>
+ * Author: Alvaro del Castillo <acs@bitergia.com>, Alberto Martín <amartin@bitergia.com>
  * MIT Licensed
 
   Generates random reviews for restaurants in orion
@@ -20,26 +20,20 @@ var fs = require('fs');
 var async = require('async');
 var shortid = require('shortid'); // unique ids generator
 var auth_request = require('../auth_request');
+var apiRestSimtasks = 2 // number of simultaneous calls to API REST
+var reviewsAdded = 0;
+var restaurantsData; // All data for the restaurants to be reviewed
 
-var api_rest_host = "compose_devguide_1";
-var api_rest_port = 80;
-var api_rest_simtasks = 2 // number of simultaneous calls to API REST
-var reviews_added = 0;
-var restaurants_data; // All data for the restaurants to be reviewed
-
-var feed_orion_reviews = function() {
-    return_post = function(res, buffer, headers) {
-        reviews_added++;
-        console.log(reviews_added+"/"+restaurants_data.length);
+var feedOrionReviews = function() {
+    returnPost = function(res, buffer, headers) {
+        reviewsAdded++;
+        console.log(reviewsAdded+"/"+restaurantsData.length);
     };
 
-    // restaurants_data = restaurants_data.slice(0,5); // debug with few items
+    // restaurantsData = restaurantsData.slice(0,5); // debug with few items
 
     console.log("Feeding reviews info in orion.");
-    console.log("Total tried: " + restaurants_data.length);
-
-    var api_rest_path = "/api/orion/entities/";
-    var org_name = "devguide";
+    console.log("Total tried: " + restaurantsData.length);
 
     // Limit the number of calls to be done in parallel to orion
     var q = async.queue(function (task, callback) {
@@ -47,16 +41,16 @@ var feed_orion_reviews = function() {
         var attributes = task.attributes;
 
         auth_request("v2/entities", "POST", attributes , callback);
-    }, api_rest_simtasks);
+    }, apiRestSimtasks);
 
 
     q.drain = function() {
-        console.log("Total reviews added: " + reviews_added);
+        console.log("Total reviews added: " + reviewsAdded);
     }
 
-    Object.keys(restaurants_data).forEach(function(element, pos, _array) {
+    Object.keys(restaurantsData).forEach(function(element, pos, _array) {
         // Call orion to append the entity
-        var rname = restaurants_data[pos].id;
+        var rname = restaurantsData[pos].id;
         rname += "-"+shortid.generate();
         // Time to add first attribute to orion as first approach
 
@@ -71,7 +65,7 @@ var feed_orion_reviews = function() {
                     "publisher": {}};
 
         attr.itemReviewed["@type"]="Restaurant";
-        attr.itemReviewed["name"]=restaurants_data[pos].id;
+        attr.itemReviewed["name"]=restaurantsData[pos].id;
 
         attr.reviewRating["@type"]="Rating";
         attr.reviewRating["ratingValue"]=utils.randomIntInc(1,5);
@@ -82,22 +76,22 @@ var feed_orion_reviews = function() {
         attr.publisher["@type"]="Organization";
         attr.publisher["name"]="Bitergia";
 
-        q.push({"rname":rname, "attributes":attr}, return_post);
+        q.push({"rname":rname, "attributes":attr}, returnPost);
     });
 };
 
 // Load restaurant data from Orion
-var load_restaurant_data = function() {
+var loadRestaurantData = function() {
 
-    var process_restaurants = function (data) {
-        restaurants_data = JSON.parse(JSON.stringify(data));
+    var processRestaurants = function (data) {
+        restaurantsData = JSON.parse(JSON.stringify(data));
         // Once we have all data for restaurants generate reviews for them
-        feed_orion_reviews();
+        feedOrionReviews();
     };
 
-    auth_request("v2/entities", "GET", { "type":"Restaurant", "limit":"1000" } , process_restaurants);
+    auth_request("v2/entities", "GET", { "type":"Restaurant", "limit":"1000" } , processRestaurants);
 };
 
 console.log("Generating random reviews for restaurants ...");
 
-load_restaurant_data();
+loadRestaurantData();
