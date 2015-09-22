d96f1723e368a4fbf46965a0924c9ae1455cc787
Alberto Martín <alberto.martin@bitergia.com>
2015-09-22 15:52:53 +0200

    Updated API to use the conversion to schema.org

--- server/routes/orionv2.js
+++ server/routes/orionv2.js
@@ -2,73 +2,72 @@
  * Orion-V2 REST API
  */
 
-var auth_request = require('../auth_request')
+var auth_request = require('../auth_request');
+var utils = require('../utils');
 
 // Restaurants
 
 exports.create_restaurant = function (req, res) {
-    auth_request("v2/entities", "POST", req.body , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities", "POST", req.body , function(data){ res.send(utils.dataToSchema(data)) });
 };
 exports.read_restaurant = function (req, res) {
-    auth_request("v2/entities", "GET", { "id":req.params.id, "type":"restaurant" } , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities/"+req.params.id, "GET", { "type":"Restaurant" } , function(data){ res.send(utils.dataToSchema(data)) });
 };
-// Update_restaurant function replaces ALL of the elements
 exports.update_restaurant = function (req, res) {
-    auth_request("v2/entities/"+req.params.id, "PUT", req.body , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities/"+req.params.id, "PATCH", req.body , function(data){ res.send(utils.dataToSchema(data)) });
 };
 exports.delete_restaurant = function (req, res) {
-    auth_request("v2/entities/"+req.params.id, "DELETE", {} , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities/"+req.params.id, "DELETE", {} , function(data){ res.send(utils.dataToSchema(data)) });
 };
 exports.get_restaurants = function(req, res) {
-    auth_request("v2/entities", "GET", { "type":"restaurant" } , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities", "GET", { "type":"Restaurant", "limit":"1000" } , function(data){ res.send(utils.dataToSchema(data)) });
 };
 
 // Reviews
 
 exports.create_review = function (req, res) {
-    auth_request("v2/entities", "POST", req.body , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities", "POST", req.body , function(data){ res.send(utils.dataToSchema(data)) });
 };
 exports.read_review = function (req, res) {
-    auth_request("v2/entities", "GET", { "id":req.params.id, "type":"review" } , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities/"+req.params.id, "GET", { "type":"Review" } , function(data){ res.send(utils.dataToSchema(data)) });
 };
-// update_review function replaces ALL of the elements
 exports.update_review = function (req, res) {
-    auth_request("v2/entities/"+req.params.id, "PUT", req.body , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities/"+req.params.id, "PATCH", req.body , function(data){ res.send(utils.dataToSchema(data)) });
 };
 exports.delete_review = function (req, res) {
-    auth_request("v2/entities/"+req.params.id, "DELETE", {} , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities/"+req.params.id, "DELETE", {} , function(data){ res.send(utils.dataToSchema(data)) });
 };
 exports.get_reviews = function(req, res) {
-    auth_request("v2/entities", "GET", { "type":"review" } , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities", "GET", { "type":"Review", "limit":"1000" } , function(data){ res.send(utils.dataToSchema(data)) });
 };
 
 // Reservations
 
 exports.create_reservation = function (req, res) {
-    auth_request("v2/entities", "POST", req.body , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities", "POST", req.body , function(data){ res.send(utils.dataToSchema(data)) });
 };
 exports.read_reservation = function (req, res) {
-    auth_request("v2/entities", "GET", { "id":req.params.id, "type":"reservation" } , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities/"+req.params.id, "GET", { "type":"FoodEstablishmentReservation" } , function(data){ res.send(utils.dataToSchema(data)) });
 };
 // update_reservation function replaces ALL of the elements
 exports.update_reservation = function (req, res) {
-    auth_request("v2/entities/"+req.params.id, "PUT", req.body , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities/"+req.params.id, "PATCH", req.body , function(data){ res.send(utils.dataToSchema(data)) });
 };
 exports.delete_reservation = function (req, res) {
-    auth_request("v2/entities/"+req.params.id, "DELETE", {} , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities/"+req.params.id, "DELETE", {} , function(data){ res.send(utils.dataToSchema(data)) });
 };
 exports.get_reservations = function(req, res) {
-    auth_request("v2/entities", "GET", { "type":"reservation" } , function(data){ res.send(JSON.stringify(data)) });
+    auth_request("v2/entities", "GET", { "type":"FoodEstablishmentReservation", "limit":"1000" } , function(data){ res.send(utils.dataToSchema(data)) });
 };
 
 // User data
 // To be fixed
 exports.get_user_reviews = function (req, res) {
-    auth_request("v2/entities", "GET", {"type":"review", "author": {"type": "Person","value": [{"name": "name","value": req.params.id}]}},
-                 function(data){ res.send(JSON.stringify(data))});
+    auth_request("v2/entities", "GET", {"type":"Review", "author": {"type": "Person","value": [{"name": "name","value": req.params.id}]}},
+                 function(data){ res.send(utils.dataToSchema(data))});
 };
 //To be fixed
 exports.get_user_reservations = function (req, res) {
-    auth_request("v2/entities", "GET", {"type":"reservation", "underName":{"type": "Person","value": [{"name": "name","value": req.params.id}]}},
-                 function(data){ res.send(JSON.stringify(data))});
+    auth_request("v2/entities", "GET", {"type":"FoodEstablishmentReservation", "underName":{"type": "Person","value": [{"name": "name","value": req.params.id}]}},
+                 function(data){ res.send(utils.dataToSchema(data))});
 };
