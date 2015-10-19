df59d69b810f6200b02fc5e82b2ae16331a863d2
David Muriel <dmuriel@bitergia.com>
2015-10-19 17:58:50 +0200

    Initial grunt configuration for js linting.

---
+++ server/Gruntfile.js
@@ -0,0 +1,30 @@
+/* jshint node: true */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // configuration
+  grunt.initConfig({
+
+    srcfiles: ['*.js', 'routes/*.js','feeders/*.js', 'idas/*.js'],
+
+    jshint: {
+      src: '<%= srcfiles %>',
+    },
+
+    jscs: {
+      src: '<%= srcfiles %>',
+    }
+  });
+
+  // load tasks
+  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // define tasks
+  grunt.registerTask('lint', 'Check source code style', ['jshint', 'jscs']);
+
+  // default task
+  grunt.registerTask('default', ['lint']);
+};
