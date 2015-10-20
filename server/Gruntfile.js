8c0641407c118ea8dea4aa0ad6f474900875c491
David Muriel <dmuriel@bitergia.com>
2015-10-20 20:28:38 +0200

    Added pre-commit hook to execute linting on server code.

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -7,6 +7,14 @@ module.exports = function(grunt) {
   // configuration
   grunt.initConfig({
 
+    // Run shell commands
+    shell: {
+      hooks: {
+        // Copy the project's pre-commit hook into .git/hooks
+        command: 'ln -sf ../git-hooks/pre-commit ../.git/hooks/'
+      }
+    },
+
     srcfiles: ['spec/*.js', '*.js', 'routes/*.js','feeders/*.js', 'idas/*.js'],
 
     jshint: {
@@ -16,14 +24,19 @@ module.exports = function(grunt) {
     jscs: {
       src: '<%= srcfiles %>',
     }
+
   });
 
   // load tasks
   grunt.loadNpmTasks('grunt-jscs');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-shell');
 
   // define tasks
   grunt.registerTask('lint', 'Check source code style', ['jshint', 'jscs']);
+
+  // Clean the .git/hooks/pre-commit file then copy in the latest version
+  grunt.registerTask('setup-git-hooks', ['shell:hooks']);
 
   // default task
   grunt.registerTask('default', ['lint']);
