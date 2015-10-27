0019d6e06e841278edd01af636ce593394bea7be
David Muriel <dmuriel@bitergia.com>
2015-10-27 22:26:03 +0100

    Updated subscription scripts for temperature and humidity sensors.

--- docker/images/fiware-devguide-app/subscription-humidity-sensors.sh
+++ docker/images/fiware-devguide-app/subscription-humidity-sensors.sh
@@ -4,18 +4,18 @@ DEVGUIDE_HOST=$( hostname -i )
 DEVGUIDE_URL=http://${DEVGUIDE_HOST}/api/orion/sensors/
 ORION_URL=http://${ORION_NO_PROXY_HOSTNAME}:${ORION_PORT}/v1/subscribeContext
 
-# IDAS temperature sensors used in restaurants
+# IDAS humidity sensors used in restaurants
 cat <<EOF | curl ${ORION_URL} -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'Fiware-Service: devguideidas' --header 'Fiware-ServicePath: /' -d @-
 {
     "entities": [
         {
             "type": "thing",
             "isPattern": "true",
-            "id": "SENSOR_TEMP_.*"
+            "id": "SENSOR_HUM_.*"
         }
     ],
     "attributes": [
-        "temperature"
+        "humidity"
     ],
     "reference": "${DEVGUIDE_URL}",
     "duration": "P1M",
