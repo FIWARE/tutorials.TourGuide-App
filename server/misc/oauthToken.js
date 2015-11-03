b36681637f4015696268339d3647c6d6a7f69a62
Alberto Martín <alberto.martin@bitergia.com>
2015-11-03 13:29:16 +0100

    Added script to retrieve an access_token given a user and pass

---
+++ server/misc/oauthToken.js
@@ -0,0 +1,48 @@
+/*
+ * oauthToken.js
+ * Copyright(c) 2015 Bitergia
+ * Author: Alberto Martín <alberto.martin@bitergia.com>
+ * MIT Licensed
+ */
+
+// jshint node: true, jasmine: true
+
+'use strict';
+
+var config = require('../config');
+var rp = require('request-promise');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+var oauthTokenUrl = config.idmUrl + '/oauth2/token';
+var username = process.argv[2];
+var password = process.argv[3];
+var auth = 'Basic ' +
+  new Buffer(config.clientId + ':' + config.clientSecret).toString('base64');
+
+var options = {
+  method: 'POST',
+  uri: oauthTokenUrl,
+  headers: {
+    'Authorization': auth,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  },
+  form: {
+    // jshint camelcase: false
+    // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+    grant_type: 'password',
+    username: username,
+    password: password,
+    client_id: config.clientId,
+    client_secret: config.clientSecret
+    // jshint camelcase: true
+    // jscs:enable
+  }
+};
+
+rp(options)
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.log(err.error);
+  });
