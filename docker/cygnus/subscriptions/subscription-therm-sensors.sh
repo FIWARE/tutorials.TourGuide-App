93b2ce462db1995eb506cfd2b7a08e3303ac112a
Alberto Martín <alberto.martin@bitergia.com>
2016-01-05 18:34:48 +0100

    Removed ACPI subscription from Devguide

--- docker/cygnus/subscriptions/subscription-therm-sensors.sh
+++ docker/cygnus/subscriptions/subscription-therm-sensors.sh
@@ -3,33 +3,7 @@
 CYGNUS_HOST=$( hostname -i )
 CYGNUS_PORT=5001
 CYGNUS_URL=http://${CYGNUS_HOST}:${CYGNUS_PORT}/notify
-ORION_URL=http://${ORION_HOSTNAME}:${ORION_PORT}/NGSI10/subscribeContext
-
-# ul20 client temperature sensors
-cat <<EOF | curl ${ORION_URL} -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'Fiware-Service: devguideiot' --header 'Fiware-ServicePath: /' -d @-
-{
-    "entities": [
-        {
-            "type": "thing",
-            "isPattern": "true",
-            "id": "SENSOR_TEMP_*"
-        }
-    ],
-    "attributes": [
-        "temperature"
-    ],
-    "reference": "${CYGNUS_URL}",
-    "duration": "P1M",
-    "notifyConditions": [
-        {
-            "type": "ONCHANGE",
-            "condValues": [
-                "TimeInstant"
-            ]
-        }
-    ]
-}
-EOF
+ORION_URL=http://${ORION_HOSTNAME}:${ORION_PORT}/v1/subscribeContext
 
 # Restaurant temperature sensors
 cat <<EOF | curl ${ORION_URL} -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'Fiware-Service: devguideidas' --header 'Fiware-ServicePath: /' -d @-
