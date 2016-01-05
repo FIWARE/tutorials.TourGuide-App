c1bf7b44ddfae3eadf2a1fd9146976709f816517
Alberto Martín <alberto.martin@bitergia.com>
2016-01-05 18:38:19 +0100

    Updated cygnus configuration file to publish in both CKAN and HDFS sinks

--- docker/cygnus/subscriptions/subscription-humidity-sensors.sh
+++ docker/cygnus/subscriptions/subscription-humidity-sensors.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 CYGNUS_HOST=$( hostname -i )
-CYGNUS_PORT=5001
+CYGNUS_PORT=5002
 CYGNUS_URL=http://${CYGNUS_HOST}:${CYGNUS_PORT}/notify
-ORION_URL=http://${ORION_NO_PROXY_HOSTNAME}:${ORION_PORT}/v1/subscribeContext
+ORION_URL=http://${ORION_HOSTNAME}:${ORION_PORT}/v1/subscribeContext
 
 # IDAS humidity sensors used in restaurants
 cat <<EOF | curl ${ORION_URL} -s -S --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'Fiware-Service: devguideidas' --header 'Fiware-ServicePath: /' -d @-
