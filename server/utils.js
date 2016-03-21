0929a19b5a37362676b94be52678893cff4e024b
Alberto Martín <alberto.martin@bitergia.com>
2016-03-21 15:46:42 +0100

    Start using 'keyValues' by default when listing elements from Orion

--- server/utils.js
+++ server/utils.js
@@ -709,15 +709,20 @@ function getOrgReservations(franchise, listOfRestaurants, listOfReservations) {
   );
 }
 
-function getListByType(type, element, headers) {
+function getListByType(type, element, headers, raw) {
   var uri = '/v2/entities';
+  var options = {'type': type,'limit': '1000'};
   if (element) {
     uri += '/' + encodeURIComponent(element);
+  }
+
+  if (typeof raw === 'undefined') {
+    options.options = 'keyValues';
   }
   return authRequest(
     uri,
     'GET',
-    {'type': type,'limit': '1000'},
+    options,
     headers
   );
 }
