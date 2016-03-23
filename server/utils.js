db8e1759846a546e07412df1e4078a8751b8b5b9
Alberto Martín <alberto.martin@bitergia.com>
2016-03-23 15:57:14 +0100

    Adapt reservation input model for Orion

--- server/utils.js
+++ server/utils.js
@@ -603,24 +603,36 @@ function reviewToOrion(userObject, schemaObject) {
 
 function reservationToOrion(userObject, schemaObject) {
 
-  // -- TODO: check automatically if there's enough space at
-  // -- the restaurant, so reservation is accepted automatically
-
   if (userObject) {
-    schemaObject = replaceTypeForOrion(schemaObject);
-    var rname = schemaObject.reservationFor.name;
-    rname += '-' + shortid.generate();
-    schemaObject.id = rname;
-    var newDate = new Date(schemaObject.startTime).getTime();
-    //Time in miliseconds to Orion
-    schemaObject.startTime = newDate;
-    schemaObject.underName = {};
-    schemaObject.partySize = parseInt(schemaObject.partySize, 10);
-    schemaObject.underName.type = 'Person';
-    schemaObject.underName.name = userObject.id;
-    schemaObject.reservationStatus = 'Confirmed';
+    var reservationId = schemaObject.reservationFor.name += '-' + shortid.generate();
+    var objectToOrion = {
+      'id': asciiEncode(reservationId),
+      'partySize': {
+        'value': schemaObject.partySize
+      },
+      'reservationFor': {
+        'type': 'FoodEstablishment',
+        'value': fixedEncodeURIComponent(schemaObject.reservationFor.name)
+      },
+      'reservationStatus': {
+        'value': 'Confirmed'
+      },
+      'startTime': {
+        'type': 'date',
+        'value': new Date(schemaObject.startTime).toISOString()
+      },
+      'address': {
+        'type': 'PostalAddress',
+        'value': schemaObject.address
+      },
+      'type': 'FoodEstablishmentReservation',
+      'underName': {
+        'type': 'Person',
+        'value': userObject.id
+      }
+    };
+    return sortObject(objectToOrion);
   }
-  return sortObject(schemaObject);
 }
 
 // filter restaurants by organization
