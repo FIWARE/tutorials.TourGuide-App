e50ff98d03b21579f4c2f4f3a360403796b36df2
Fiware ULPGC <fiware@ulpgc.es>
2016-03-31 11:23:21 +0100

    Client style improvements:  - Nomenclature changes (camelCase, use of english, renamed files, etc).  - Translate to DOM syntax.  - Process reviews onsubmit instead of onclick.  - DOM reservationForm. Fixed disabled/enabled form elements.  - Deleted commented code. Delete unnecesary code.  - Removed several console.log lines.  - Used innerHTML= '' instead of removeChild.  - Module pattern implemented.  - Added doctype to HTML files. CSS and JS inclusions have been ordered.  - Fixed authors text.  - Changes to pass linter.  - Roles added as properties.  - Client structure changed.      - Moved bootstrap files to js folder.      - Moved app.css to stylesheets folder.  - Usage of forEach.  - Usage of try-catch avoided.  - Usage of constants.  - Deleted proxy variable.  - Other minor style fixes.

--- client/js/myReviews.js
+++ client/js/myReviews.js
@@ -3,24 +3,24 @@
  * myReviews.js
  * Copyright(c) 2016 Universidad de Las Palmas de Gran Canaria
  * Authors:
- *   Jaisiel Santana <jaisiel@gmail.com>,
+ *   Jaisiel Santana <jaisiel@gmail.com>
  *   Alejandro Sánchez <alemagox@gmail.com>
  *   Pablo Fernández <pablo.fernandez@ulpgc.es>
  * MIT Licensed
 
 */
 //initialization
-var init_reviews = function() {
-  //only gets reviews if the user is loged
-  login_needed(function() {
+var initReviews = function() {
+  //only gets reviews if the user is logged
+  connectionsAPI.loginNeeded(function() {
     var userInfo = JSON.parse(localStorage.getItem('userInfo'));
-    get_user_reviews(userInfo.displayName);
+    restaurantsAPI.getUserReviews(userInfo.displayName);
   });
 
 
   //todo translate to common js
   $('tbody').height($(window).height() - $('thead th').height() -
-    $('#logged_div').height() - 50);
+    $('#loggedDiv').height() - 50);
 };
 
-addLoadEvent(init_reviews);
+utils.addLoadEvent(initReviews);
