12c6575aae66c4e259486f217217db05612eeafb
Alberto Martín <alberto.martin@bitergia.com>
2016-08-03 16:49:45 +0200

    Split restaurantfeeder.js into a single feeder and a geocoder script

--- server/feeders/restaurantfeeder.js
+++ server/feeders/restaurantfeeder.js
@@ -4,106 +4,180 @@
  * Author: Alvaro del Castillo <acs@bitergia.com>,
  * Alberto Martín <alberto.martin@bitergia.com>
  * MIT Licensed
-
-  Feeds restaurants into Orion CB
-
-  First it gets all restaurant information
-  Then restaurant geocode is gathered using Google API in serial execution
-  Then all restaurant data is added to Orion CB
+ *
+ *  Feeds restaurants into Orion CB
+ *
+ *  First it gets all restaurant information from the JSON file and
+ *  the geolocation objects from the GEO JSON file
+ *  Then all restaurant data is added to Orion CB
 */
 
 // jshint node: true
 
 'use strict';
 
+var path = require('path');
 var utils = require('../utils');
-var fs = require('fs');
-var async = require('async');
-var geocoder = require('node-geocoder')('google', 'http');
-
-var feedHost = 'opendata.euskadi.eus';
-var feedPath = '/contenidos/ds_recursos_turisticos';
-feedPath +=
-  '/restaurantes_sidrerias_bodegas/opendata/restaurantes.json';
-var cacheFile = '../data/restaurants.json';
-var apiRestSimtasks = 1; // number of simultaneous calls to API REST
-var restaurantsAdded = 0;
-var geoWaitTimeMs = 200; // Wait ms between calls to Google API
-var restaurantsData; // All data for the restaurants
-var apiCount = 0; // 2500 requests per day, be careful
-
 var config = require('../config');
+var fs = require('fs');
+
+var RESTAURANT_TYPE = 'Restaurant';
+var POSTAL_ADDRESS_TYPE = 'PostalAddress';
+var PROPERTY_VALUE_TYPE = 'PropertyValue';
+var DATE_TYPE = 'DateTime';
+var AGGREGATE_RATING_TYPE = 'AggregateRating';
+
+var cacheFile = '../data/restaurants.json';
+var geolocationFile = '../data/restaurantsGeo.json';
+var restaurantsAdded = 0;
+var restaurantsData; // All data for the restaurants
+var geoData;
+var restaurantSelected;
+var numberOfRestaurants;
+
 var fiwareHeaders = {
   'fiware-service': config.fiwareService
 };
 
-var getAddress = function(restaurant) {
-  var address = restaurant.address.value.streetAddress + ' ';
-  if (restaurant.address.value.addressLocality) {
-    address += restaurant.address.value.addressLocality + ' ';
-  }
-  if (restaurant.address.value.addressRegion) {
-    address += restaurant.address.value.addressRegion;
-  }
-  return address;
-};
+/**
+ * Function to display a help menu using the CLI
+*/
+function showHelp() {
+  var progname = path.basename(process.argv[1]);
+  // jshint multistr: true, maxlen: false
+  // jscs:disable maximumLineLength, disallowMultipleLineStrings
+  var helpMessage = [
+    '',
+    'Generate restaurants based in Open Euskadi JSON and load it into Orion Context Broker.',
+    '',
+    'Usage: ' + progname + ' <options>',
+    '',
+    'Available options:',
+    '',
+    '  -h  --help                          Show this help.',
+    '  -f  --jsonfile                      Use a different JSON data from Open Euskadi.',
+    '                                      (Feeder expects the same format).',
+    '                                      It use by default the one provided in ',
+    '                                      \'server/data/restaurants.json\'. ',
+    '  -g  --geofile                       Use a different JSON data for restaurant Geolocation. ',
+    '                                      It use by default the one provided in ',
+    '                                      \'server/data/restaurantsGeo.json\' (retrieved with Google).',
+    '  -r  --restaurant                    Load a single restaurant of the JSON provided using its name.',
+    '  -n  --numberOfRestaurants           Load a defined number of restaurants among the ',
+    '                                      provided ones (can\'t be used with the previous option).',
+    ''].join('\n');
+  // jshint multistr: false, maxlen: 80
+  // jscs:enable
+  console.log(helpMessage);
+  process.exit(0);
+}
 
-var feedOrionRestaurants = function() {
-  var returnPost = function(restaurantIdentifier) {
-    if (restaurantIdentifier) {
-      console.log('The restaurant', restaurantIdentifier, 'exists');
-    } else {
-      restaurantsAdded++;
-      console.log(restaurantsAdded + '/' + restaurantsData.length);
+/**
+ * Function to parse the arguments given launching the script
+*/
+function parseArgs() {
+  if (process.argv.length < 3) {
+    // no args, use defaults
+    return;
+  }
+
+  var argv = require('minimist')(process.argv.slice(2), {
+    alias: {
+      h: 'help',
+      f: 'jsonfile',
+      g: 'geofile',
+      r: 'restaurant',
+      n: 'numberOfRestaurants'
+    },
+    default: {
+      'jsonfile': '../data/restaurants.json',
+      'geofile': '../data/restaurantsGeo.json'
     }
-  };
+  });
 
-  //restaurantsData = restaurantsData.slice(0,5); // debug with few items
+  if (argv.help) {
+    showHelp();
+  }
 
-  console.log('Feeding restaurants info in orion.');
-  console.log('Number of restaurants: ' + restaurantsData.length);
+  if (typeof argv.jsonfile === 'string' &&
+      argv.jsonfile !== '') {
+    cacheFile = argv.jsonfile;
+  }
 
-  // Limit the number of calls to be done in parallel to orion
-  var q = async.queue(function(task, callback) {
-    var attributes = task.attributes;
-    var fwHeaders = JSON.parse(JSON.stringify(fiwareHeaders));
-    utils.getListByType('Restaurant', attributes.id, fwHeaders)
-    .then(function(data) {
-      callback(attributes.id);
-    })
-    .catch(function(err) {
-      if (err.statusCode == '404') {
-        var address = getAddress(attributes);
-        if (attributes.department.value) {
-          fwHeaders['fiware-servicepath'] = '/' + attributes.department.value;
-        }
-        setTimeout(function() {
-         geocoder.geocode({address: address, country: 'Spain'})
-         .then(function(geoRes) {
-          attributes = utils.addGeolocation(attributes, geoRes[0]);
-          attributes = utils.completeAddress(attributes, geoRes[0]);
-          utils.sendRequest('POST', attributes, null, fwHeaders)
-          .then(callback(null))
-          .catch(function(err) {
-            console.error(err);
-          });
-        })
-         .catch(function(err) {
-          console.error(err);
-          utils.sendRequest('POST', attributes, null, fwHeaders)
-          .then(callback(null))
-          .catch(function(err) {
-            console.error(err);
-          });
-        });
-       }, geoWaitTimeMs);
+  if (typeof argv.geofile === 'string' &&
+      argv.geofile !== '') {
+    geolocationFile = argv.geofile;
+  }
+
+  if (typeof argv.restaurant === 'string' &&
+      argv.restaurant !== '') {
+    restaurantSelected = argv.restaurant;
+  }
+
+  if (typeof argv.numberOfRestaurants === 'number' &&
+      argv.numberOfRestaurants !== '') {
+    numberOfRestaurants = argv.numberOfRestaurants;
+  }
+
+  if (argv.restaurant && argv.numberOfRestaurants) {
+    console.error('\'restaurant\' and \'numberOfRestaurants\' ' +
+                  'are incompatible. Please try again.');
+    showHelp();
+    process.exit(1);
+  }
+}
+
+/**
+ * Function that logs the progress of the restaurants loading
+ *
+ * @param {String} restaurantIdentifier - Restaurant identifier
+*/
+function logProgress(restaurantIdentifier) {
+  if (restaurantIdentifier) {
+    console.warn('The restaurant', restaurantIdentifier, 'exists');
+  } else {
+    restaurantsAdded++;
+    console.info(restaurantsAdded + '/' + restaurantsData.length);
+  }
+}
+
+/**
+ * Function to read the restaurant information from a given file
+*/
+function loadRestaurantData() {
+
+  console.info('Loading restaurants info ...');
+
+  var data = fs.readFileSync(cacheFile);
+  restaurantsData = JSON.parse(data);
+
+  if (restaurantSelected) {
+    restaurantsData = restaurantsData.filter(function(element) {
+      if (restaurantSelected === element.documentName) {
+        return true;
       }
     });
-  }, apiRestSimtasks);
+  }
+  if (numberOfRestaurants) {
+    restaurantsData = restaurantsData.slice(0,numberOfRestaurants);
+  }
+}
 
-  q.drain = function() {
-    console.log('Total restaurants added: ' + restaurantsAdded);
-  };
+/**
+ * Function to read the Geolocation information from a given file
+*/
+function loadGeoData() {
+  var data = fs.readFileSync(geolocationFile);
+  geoData = JSON.parse(data);
+}
+
+/**
+ * Function that adapts the information for Orion Context Broker
+*/
+function feedOrionRestaurants() {
+
+  console.info('Feeding restaurants info in orion.');
+  console.info('Number of restaurants: ' + restaurantsData.length);
 
   var dictionary = {
     'id': 'name',
@@ -121,21 +195,21 @@ var feedOrionRestaurants = function() {
     'municipalityCode': 'postalCode'
   };
 
-  Object.keys(restaurantsData).forEach(function(element, index) {
+  var organization = ['Franchise1', 'Franchise2',
+  'Franchise3', 'Franchise4'
+  ];
+  var capacity = [50, 80, 100, 120, 160, 200];
+
+  restaurantsData.forEach(function(element, index) {
 
     var rname = utils.fixedEncodeURIComponent(
       restaurantsData[index].documentName);
 
-    var organization = ['Franchise1', 'Franchise2',
-    'Franchise3', 'Franchise4'
-    ];
-    var capacity = [50, 80, 100, 120, 160, 200];
-
     var attr = {
-      'type': 'Restaurant',
+      'type': RESTAURANT_TYPE,
       'id': utils.generateId(rname),
       'address': {
-        'type': 'PostalAddress',
+        'type': POSTAL_ADDRESS_TYPE,
         'value': {}
       },
       'name': {
@@ -145,11 +219,11 @@ var feedOrionRestaurants = function() {
         'value': utils.randomElement(organization)
       },
       'capacity': {
-        'type': 'PropertyValue',
+        'type': PROPERTY_VALUE_TYPE,
         'value': utils.randomElement(capacity)
       },
       'aggregateRating': {
-        'type': 'AggregateRating',
+        'type': AGGREGATE_RATING_TYPE,
         'value': {
           'ratingValue': utils.randomIntInc(1, 5),
           'reviewCount': utils.randomIntInc(1, 100)
@@ -158,11 +232,11 @@ var feedOrionRestaurants = function() {
       'occupancyLevels': {
         'metadata': {
           'timestamp': {
-            'type': 'DateTime',
+            'type': DATE_TYPE,
             'value': new Date().toISOString()
           }
         },
-        'type': 'PropertyValue',
+        'type': PROPERTY_VALUE_TYPE,
         'value': 0
       }
     };
@@ -174,19 +248,11 @@ var feedOrionRestaurants = function() {
       if (val) {
         if (element in addressDictionary) {
           element = utils.fixedEncodeURIComponent(
-            utils.replaceOnceUsingDictionary(
-              addressDictionary, element,
-              function(key, dictionary) {
-                return dictionary[key];
-              }));
+            utils.replaceOnceUsingDictionary(addressDictionary, element));
           attr.address.value[element] = utils.fixedEncodeURIComponent(val);
         } else if (element in dictionary) {
           element = utils.fixedEncodeURIComponent(
-            utils.replaceOnceUsingDictionary(
-              dictionary, element,
-              function(key, dictionary) {
-                return dictionary[key];
-              }));
+            utils.replaceOnceUsingDictionary(dictionary, element));
           if (element == 'priceRange') {
             attr[element] = {
               'value': parseFloat(val)
@@ -200,45 +266,38 @@ var feedOrionRestaurants = function() {
         }
       }
     });
-    q.push({'attributes': attr}, returnPost);
+    sendRestaurant(attr);
   });
-};
+}
 
-// Load restaurant data in Orion
-var loadRestaurantData = function() {
-  var processGet = function(res, data) {
-    fs.writeFileSync(cacheFile, data);
-    restaurantsData = JSON.parse(data);
-    feedOrionRestaurants();
-  };
-
-  try {
-    var data = fs.readFileSync(cacheFile);
-    console.log('Using cache file ' + cacheFile);
-    restaurantsData = JSON.parse(data);
-    feedOrionRestaurants();
-  } catch (e) {
-    if (e.code === 'ENOENT') {
-      console.log('Downloading data ... be patient');
-
-      var headers = {
-        'Accept': 'application/json',
-      };
-
-      var options = {
-        host: feedHost,
-        path: feedPath,
-        method: 'GET',
-        headers: headers
-      };
-      utils.doGet(options, processGet);
-
-    } else {
-      throw e;
+/**
+ * Function that sends the information to Orion Context Broker
+ *
+ * @param {String} restaurant - Restaurant to load
+*/
+function sendRestaurant(restaurant) {
+  var fwHeaders = JSON.parse(JSON.stringify(fiwareHeaders));
+  utils.getListByType('Restaurant', restaurant.id, fwHeaders)
+  .then(function(data) {
+    logProgress(restaurant.id);
+  })
+  .catch(function(err) {
+    if (err.statusCode == '404') {
+      if (restaurant.department.value) {
+        fwHeaders['fiware-servicepath'] = '/' + restaurant.department.value;
+      }
+      restaurant = utils.addGeolocation(restaurant, geoData[restaurant.id]);
+      restaurant = utils.completeAddress(restaurant, geoData[restaurant.id]);
+      utils.sendRequest('POST', restaurant, null, fwHeaders)
+      .then(logProgress(null))
+      .catch(function(err) {
+        console.error(err);
+      });
     }
-  }
-};
+  });
+}
 
-console.log('Loading restaurants info ...');
-
+parseArgs();
 loadRestaurantData();
+loadGeoData();
+feedOrionRestaurants();
